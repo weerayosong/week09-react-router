1c5e39768242e1357794babb62b1e57ef6de559e
weerayosong <weerayosong@outlook.com>
2026-04-27 12:11:34 +0700

    chore: study react router via build pages

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,15 @@
+import { Outlet } from 'react-router-dom'
+
 export default function Layout() {
-    return <div>Navbar</div>
+    return (
+        <div className="container mx-auto max-w-100">
+            <div className="flex justify-between bg-lime-400 ">
+                <div className="font-bold px-1">Logo</div>
+                <div className="italic px-1">Navbar</div>
+            </div>
+            <div className="bg-slate-200 shadow-md">
+                <Outlet />
+            </div>
+        </div>
+    )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import { Link, useNavigate } from 'react-router-dom'
+
+export default function Home() {
+    const navigate = useNavigate()
+
+    const handleNavigate = () => {
+        navigate('/about')
+    }
+
+    return (
+        <div>
+            <h1 className="text-center text-2xl font-bold">Home Page</h1>
+            <button
+                className="p-1 m-2 rounded-sm font-bold text-white  bg-purple-600 hover:bg-purple-300"
+                onClick={handleNavigate}
+            >
+                Button to About
+            </button>
+            <Link to="/about">
+                <span className="p-1 border font-bold border-purple-600 rounded-sm hover:bg-purple-300">
+                    Link to About
+                </span>
+            </Link>
+        </div>
+    )
+}
